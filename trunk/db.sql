4db2dde4fcd4fb847ddaead9bdf91b77a352d543
pekka.jarvinen <pekka.jarvinen@d902053c-ff6d-11dd-984d-ab748d24aa7e>
2009-02-26 16:43:22 +0000

    Updated database

--- trunk/db.sql
+++ trunk/db.sql
@@ -3,14 +3,14 @@
 -- http://www.phpmyadmin.net
 --
 -- Palvelin: localhost
--- Luontiaika: 20.02.2009 klo 22:18
+-- Luontiaika: 26.02.2009 klo 18:47
 -- Palvelimen versio: 5.0.67
 -- PHP:n versio: 5.2.6-2ubuntu4
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Tietokanta: `pv`
+-- Tietokanta: `zfce`
 --
 
 -- --------------------------------------------------------
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `AUTHORS` (
   `email` varchar(255) collate utf8_swedish_ci NOT NULL,
   `password` varchar(32) collate utf8_swedish_ci NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci CHECKSUM=1;
 
 -- --------------------------------------------------------
 
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `COMICS` (
   PRIMARY KEY  (`id`),
   KEY `fk_COMICS_AUTHORS` (`authorid`),
   KEY `fk_COMICS_AUTHORS1` (`idea`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci CHECKSUM=1;
 
 -- --------------------------------------------------------
 
@@ -60,15 +60,19 @@ CREATE TABLE IF NOT EXISTS `COMICS` (
 
 CREATE TABLE IF NOT EXISTS `COMMENTS` (
   `id` int(11) NOT NULL auto_increment,
-  `nick` varchar(255) character set utf8 collate utf8_swedish_ci NOT NULL,
-  `comment` varchar(255) character set utf8 collate utf8_swedish_ci NOT NULL,
+  `nick` varchar(255) collate utf8_swedish_ci NOT NULL,
+  `comment` varchar(255) collate utf8_swedish_ci NOT NULL,
   `comicid` int(11) NOT NULL,
   `added` datetime NOT NULL,
   `isstaff` int(11) NOT NULL default '0',
   `rate` int(11) default NULL,
+  `country` varchar(32) collate utf8_swedish_ci NOT NULL default 'unknown',
+  `ipaddr` varchar(255) collate utf8_swedish_ci NOT NULL,
+  `useragent` text collate utf8_swedish_ci NOT NULL,
+  `isspam` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`id`),
   KEY `fk_COMMENTS_COMICS` (`comicid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci CHECKSUM=1;
 
 -- --------------------------------------------------------
 
@@ -78,13 +82,17 @@ CREATE TABLE IF NOT EXISTS `COMMENTS` (
 
 CREATE TABLE IF NOT EXISTS `GUESTBOOK` (
   `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) character set utf8 collate utf8_swedish_ci default NULL,
-  `email` varchar(255) character set utf8 collate utf8_swedish_ci default NULL,
-  `question` text character set utf8 collate utf8_swedish_ci NOT NULL,
-  `answer` text character set utf8 collate utf8_swedish_ci,
+  `name` varchar(255) collate utf8_swedish_ci default NULL,
+  `email` varchar(255) collate utf8_swedish_ci default NULL,
+  `question` text collate utf8_swedish_ci NOT NULL,
+  `answer` text collate utf8_swedish_ci,
   `added` datetime NOT NULL,
+  `country` varchar(32) collate utf8_swedish_ci NOT NULL,
+  `ipaddr` varchar(255) collate utf8_swedish_ci NOT NULL,
+  `useragent` text collate utf8_swedish_ci NOT NULL,
+  `isspam` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci CHECKSUM=1;
 
 -- --------------------------------------------------------
 
@@ -93,12 +101,12 @@ CREATE TABLE IF NOT EXISTS `GUESTBOOK` (
 --
 
 CREATE TABLE IF NOT EXISTS `PAGES` (
-  `name` varchar(255) character set utf8 collate utf8_swedish_ci NOT NULL,
-  `content` longtext character set utf8 collate utf8_swedish_ci NOT NULL,
+  `name` varchar(255) collate utf8_swedish_ci NOT NULL,
+  `content` longtext collate utf8_swedish_ci NOT NULL,
   `updated` datetime NOT NULL,
   `added` datetime NOT NULL,
   PRIMARY KEY  (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci CHECKSUM=1;
 
 -- --------------------------------------------------------
 
@@ -108,13 +116,13 @@ CREATE TABLE IF NOT EXISTS `PAGES` (
 
 CREATE TABLE IF NOT EXISTS `POSTS` (
   `id` int(11) NOT NULL auto_increment,
-  `subject` text character set utf8 collate utf8_swedish_ci NOT NULL,
-  `content` longtext character set utf8 collate utf8_swedish_ci NOT NULL,
+  `subject` text collate utf8_swedish_ci NOT NULL,
+  `content` longtext collate utf8_swedish_ci NOT NULL,
   `authorid` int(11) NOT NULL,
   `added` datetime NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `fk_POSTS_AUTHORS` (`authorid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci CHECKSUM=1;
 
 --
 -- Rajoitteet vedostauluille
